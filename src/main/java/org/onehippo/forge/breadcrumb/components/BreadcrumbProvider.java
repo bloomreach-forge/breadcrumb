26247a76de0f222c8ecc165eaeb5cff5e7b06074
Jeroen Hoffman <jeroen.hoffman@bloomreach.com>
2018-01-10 12:03:46 +0100

    HIPFORGE-127 add content based items only if links and not /pagenotfound

--- src/main/java/org/onehippo/forge/breadcrumb/components/BreadcrumbProvider.java
+++ src/main/java/org/onehippo/forge/breadcrumb/components/BreadcrumbProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2017 Hippo B.V. (http://www.onehippo.com)
+ * Copyright 2009-2018 Hippo B.V. (http://www.onehippo.com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -404,7 +404,7 @@ public class BreadcrumbProvider {
         // go up to until site content base bean
         while (!bean.isSelf(siteContentBean)){
             final BreadcrumbItem item = getBreadcrumbItem(request, bean);
-            if (item != null) {
+            if ((item != null) && (!item.getLink().isNotFound())) {
                 items.add(item);
             }
             bean = bean.getParentBean();
